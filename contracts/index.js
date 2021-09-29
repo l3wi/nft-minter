90c1f3a82a5190669ca6eeb1cde63ea25ff315c4
Lewis Freiberg <1716917+l3wi@users.noreply.github.com>
2021-09-29 20:57:18 +0200

    Separate minter and edition abis

--- contracts/index.js
+++ contracts/index.js
@@ -1,3 +1,15 @@
-import usdcInfo from './usdc'
+import usdcInfo from "./usdc"
+import zoraMinterABI from "./zoraMinter.json"
+import zoraNFTABI from "./zoraNFT.json"
+
+export const zoraMinter = {
+  abi: zoraMinterABI,
+  1: "0xEf7a8fF7FC585FA1Fe3062455B628bfC657e2cD5",
+  4: "0x5d6E1357Acc8BF654979f3b24fdef8C5549A491e"
+}
+
+export const zoraNFT = {
+  abi: zoraNFTABI
+}
 
 export const usdc = usdcInfo
